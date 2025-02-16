2d650e2bc255fa5ea4787268e21b5ac9b600e4a4
AnonymousDEV001 <126903535+AnonymousDEV001@users.noreply.github.com>
2025-02-16 18:38:56 +0500

    fix responsiveness

--- src/app/donor/page.jsx
+++ src/app/donor/page.jsx
@@ -4,9 +4,9 @@ import DonorForm from "@/components/Forms/DonorForm/DonorForm";
 const page = async () => {
     const countries = await getCountries();
   return (
-    <div className="flex justify-center items-center gap-20 text-sm m-8">
+    <div className="flex justify-center flex-wrap items-center gap-20 text-sm m-8">
       <img src="/banner.png" className="h-full" alt="" />
-      <div className="w-full  md:w-[30%] py-12">
+      <div className="py-12">
         <DonorForm countries={countries}/>
       </div>
     </div>
--- src/app/recipient/page.jsx
+++ src/app/recipient/page.jsx
@@ -5,9 +5,9 @@ import RecipientForm from "@/components/Forms/RecipientForm/RecipientForm";
 const page = async () => {
   const countries = await getCountries();
   return (
-    <div className="flex justify-center items-center gap-20 text-sm">
+    <div className="flex justify-center flex-wrap items-center gap-20 text-sm m-8">
       <img src="/banner.png" className="h-full" alt="" />
-      <div className="w-[30%] py-12">
+      <div className="w-full lg:w-[30%] py-12">
         <RecipientForm countries={countries}/>
       </div>
     </div>
